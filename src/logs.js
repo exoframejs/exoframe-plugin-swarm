25328abdc2b18bbf0256280627058efa99c86cba
Tim Ermilov <yamalight@gmail.com>
2019-02-11 11:51:57 +0100

    Add logs fetching for exoframe-server

--- src/logs.js
+++ src/logs.js
@@ -2,8 +2,24 @@ const {Readable} = require('stream');
 
 module.exports = async ({docker, _, logsConfig, fixLogStream, username, id, reply, follow}) => {
   const allServices = await docker.listServices();
-  const serviceInfo = allServices.find(c => c.Spec.Labels['exoframe.user'] === username && c.Spec.Name === id);
+  const serverInfo = allServices.find(c => c.Spec.Name.includes('exoframe-server'));
 
+  // if user asked for server logs - just send them back
+  if (id === 'exoframe-server') {
+    // if not running in container - just notify user
+    if (!serverInfo) {
+      const logStream = fixLogStream(`${new Date().toISOString()} Exoframe server not running in container!`);
+      reply.send(logStream);
+      return;
+    }
+    const service = docker.getService(serverInfo.ID);
+    const logs = await service.logs(logsConfig);
+    const logStream = fixLogStream(logs);
+    reply.send(logStream);
+    return;
+  }
+
+  const serviceInfo = allServices.find(c => c.Spec.Labels['exoframe.user'] === username && c.Spec.Name === id);
   if (serviceInfo) {
     const service = docker.getService(serviceInfo.ID);
     const logs = await service.logs(logsConfig);
